ee0e7e55c1942cfba681d8d611679d10000a5536
ortub10 <ortub10@gmail.com>
2022-12-26 15:35:15 +0200

    fetch

---
+++ l2/js/bit.js
@@ -0,0 +1,16 @@
+class CryptoClass {
+  constructor(_parent, _name, _price) {
+    this.parent = _parent;
+    this.name = _name;
+    this.price = _price;
+  }
+
+  renderToHtml() {
+    let newDiv = document.createElement("div");
+    newDiv.className = "col-6 border p-3";
+    document.querySelector(this.parent).append(newDiv);
+
+    newDiv.innerHTML += `<h3>${this.name}</h3>
+    <div>Value: ${this.price} USD</div>`;
+  }
+}
---
+++ l2/js/main.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+  doRestApi();
+};
+
+const doRestApi = () => {
+  let myUrl = "http://fs1.co.il/bus/bitcoin.php";
+  fetch(myUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      data.map((item) => {
+        let coin = new CryptoClass("#id_row", item.name, item.price_usd);
+        coin.renderToHtml();
+      });
+    });
+};
