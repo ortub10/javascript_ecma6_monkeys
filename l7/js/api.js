753c96e6da70b067f1838b6a67c7216233f4e298
ortub10 <ortub10@gmail.com>
2022-12-27 11:04:30 +0200

    axios all

--- l7/js/api.js
+++ l7/js/api.js
@@ -3,10 +3,25 @@ const init = () => {
 };
 
 const doApi = async () => {
-  let url = " http://fs1.co.il/bus/xbox2.php";
-  let resp = await axios.get(url);
+  let url = " http://fs1.co.il/bus/xbox1.php";
+  let url2 = " http://fs1.co.il/bus/xbox2.php";
+
+  let urls_ar = [url, url2];
+  let gets_ar = urls_ar.map((getUrl) => {
+    return axios.get(getUrl);
+  });
+
+  let resp = await axios.all(gets_ar);
+  let ar = [];
+  resp.forEach((item) => {
+    ar.push(...item.data);
+  });
+  console.log(resp);
+  console.log(ar);
+
+  // let resp = await axios.get(url);
   // console.log(resp.data);
-  createList(resp.data);
+  // createList(resp.data);
 };
 
 const createList = (_ar) => {
