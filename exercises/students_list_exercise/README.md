8a5db4465c894d8c27432a033855ddcb04bfd60b
ortub10 <ortub10@gmail.com>
2022-12-27 14:24:08 +0200

    students list exercise - part 2 - verifying and local storage

--- exercises/students_list_exercise/README.md
+++ exercises/students_list_exercise/README.md
@@ -5,3 +5,12 @@ Creating UI of form
 ![part1](https://user-images.githubusercontent.com/102150516/209663897-e4185fc0-ae87-4d65-ac62-67cfc3a4d5be.png)
 
 ---
+
+## part 2:
+
+Verifying the form and saving it to local storage
+
+![part2_a](https://user-images.githubusercontent.com/102150516/209666175-498d5e27-b723-4f2f-a906-cb23d737ac6d.png)
+![part2_b](https://user-images.githubusercontent.com/102150516/209666171-ee0184ca-2a6a-4bc0-bd7a-a49e4f38b509.png)
+
+---
--- exercises/students_list_exercise/js/studentForm.js
+++ exercises/students_list_exercise/js/studentForm.js
@@ -1,3 +1,5 @@
+let local_ar = [];
+
 const init = () => {
   declareViewEvents();
 };
@@ -6,6 +8,21 @@ const declareViewEvents = () => {
   let form = document.querySelector("#id_form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
+    let student_obj = {
+      name: document.querySelector("#name").value,
+      score: document.querySelector("#score").value,
+      subject: document.querySelector("#subject").value,
+    };
+    if (student_obj.name.length < 2) {
+      return alert("Name too short, min 2 chars");
+    } else if (student_obj.score < 1 || student_obj.score > 100) {
+      return alert("Score need to be between 1 to 100");
+    }
+    // console.log(student_obj);
+    local_ar.push(student_obj);
+
+    localStorage.setItem("students", JSON.stringify(local_ar));
+    // console.log(local_ar);
   });
 };
 init();
